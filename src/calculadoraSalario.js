3ee1e5121c55f55b49fb968943a3c3d4e871224d
TiagoPerri <tiagocnsr@gmail.com>
2021-10-12 01:53:06 -0300

    Passo 2 - Ciclo TDD - DEV

--- src/calculadoraSalario.js
+++ src/calculadoraSalario.js
@@ -1,5 +1,15 @@
 function calculadoraSalario(valorSal, cargo){
-  
+    function formulaSalario(valorSal, porcentagem){
+        return valorSal - ((valorSal * porcentagem) / 100);
+    }
+
+    switch(cargo){
+        case "Desenvolvedor":
+            if(valorSal >= 3000){
+                return formulaSalario(valorSal, 20);
+            }
+        break;
+    }
 }
 
 module.exports = calculadoraSalario;
